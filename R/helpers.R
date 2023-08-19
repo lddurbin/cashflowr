ef332dcc62e88152ca95bc67d89eb8fb11062ee9
lddurbin <l.d.durbin@gmail.com>
2023-08-19 16:11:39 +1200

    New function and test for net income. Removed references to googlesheets4 package.

--- R/helpers.R
+++ R/helpers.R
@@ -2,6 +2,8 @@
 #'
 #' @param data A tibble with three columns: item, type, and value.
 #' @param type The budget type, either "operating_spend" or "income".
+#' @param period Should the values be calculated as monthly or fortnightly?
+#'   Default is monthly.
 #'
 #' @return A numeric value.
 #'
@@ -16,7 +18,7 @@
 #'
 #' # Sum operating spend:
 #' get_total(test_data, type = "operating_spend")
-get_total <- function(data, type) {
+get_total <- function(data, type, period = "monthly") {
   total <- value <- NULL
 
   total_spend <- data |>
@@ -24,6 +26,37 @@ get_total <- function(data, type) {
     filter(type == {{type}}) |>
     pull(total)
 
-  return(total_spend)
+  total_spend_per_period <- dplyr::if_else(
+    period == "monthly",
+    total_spend,
+    (total_spend*12)/26
+  )
+
+  return(total_spend_per_period)
 }
 
+
+#' Calculate Income After Operating Spend
+#'
+#' @param data A tibble with three columns: item, type, and value.
+#'
+#' @return A numeric value.
+#' @export
+#'
+#' @examples
+#' test_data <- tibble::tibble(
+#'   item = c("Item 1", "Item 2", "Item 3", "Item 1", "Item 2"),
+#'   type = c(rep("operating_spend", 3), rep("income", 2)),
+#'   value = c(100,250,50,1000,1200)
+#' )
+#'
+#' #Total net income:
+#' get_net_income(test_data)
+get_net_income <- function(data) {
+  total_income <- get_total(data, "income")
+  total_operating_spend <- get_total(data, "operating_spend")
+
+  net_income <- total_income - total_operating_spend
+
+  return(net_income)
+}
--- tests/testthat/test-helpers.R
+++ tests/testthat/test-helpers.R
@@ -1,7 +1,4 @@
 test_that("we can return total operating spend and total income", {
-  googlesheets4::gs4_deauth()
-  googlesheets4::gs4_user()
-
   test_data <- tibble::tibble(
     item = c("Item 1", "Item 2", "Item 3", "Item 1", "Item 2"),
     type = c(rep("operating_spend", 3), rep("income", 2)),
@@ -10,4 +7,15 @@ test_that("we can return total operating spend and total income", {
 
   expect_equal(get_total(test_data, "operating_spend"), 400)
   expect_equal(get_total(test_data, "income"), 2200)
+})
+
+
+test_that("we can return total net income", {
+  test_data <- tibble::tibble(
+    item = c("Item 1", "Item 2", "Item 3", "Item 1", "Item 2"),
+    type = c(rep("operating_spend", 3), rep("income", 2)),
+    value = c(100,250,50,1000,1200)
+  )
+
+  expect_equal(get_net_income(test_data), 1800)
 })
